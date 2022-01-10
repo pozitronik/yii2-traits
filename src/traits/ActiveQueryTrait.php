8d8bdb74a2499c1a71a3e73b59e71b1b2a00baef
Pozitronik <antikillerxxl@gmail.com>
2022-01-10 16:16:32 +0300

    Inspection suppressed with commentary

--- src/traits/ActiveQueryTrait.php
+++ src/traits/ActiveQueryTrait.php
@@ -18,6 +18,7 @@ trait ActiveQueryTrait {
 	 * @param bool $deleted
 	 * @return $this
 	 * @example ActiveRecord::find()->active()->all()
+	 * @noinspection PhpDocSignatureInspection Корректно: нет способа объяснить инспектору, что трейт привязан к ActiveQuery
 	 */
 	public function active(bool $deleted = false):self {
 		/** @var ActiveRecord $class */
