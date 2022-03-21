dd261442693630d2d1d9896299a3839ed3f7d275
Pozitronik <antikillerxxl@gmail.com>
2022-03-21 18:14:27 +0300

    link method (to() method alternative)

--- src/traits/ControllerTrait.php
+++ src/traits/ControllerTrait.php
@@ -8,6 +8,7 @@ use pozitronik\helpers\Utils;
 use ReflectionException;
 use yii\base\UnknownClassException;
 use yii\helpers\Url;
+use yii\web\Controller;
 
 /**
  * Трейт для аугментации контроллеров
@@ -33,7 +34,8 @@ trait ControllerTrait {
 	}
 
 	/**
-	 * По имени экшена возвращает строковой абсолютный Url в приложении
+	 * По имени экшена возвращает строковой абсолютный Url в приложении.
+	 * Не учитывает, находится ли контроллер в модуле (использовать Module::to() или $controller->link())
 	 * @param string $action
 	 * @param array $params
 	 * @param bool|string $scheme @see Url::to() $scheme parameter
@@ -42,6 +44,21 @@ trait ControllerTrait {
 	 */
 	public static function to(string $action, array $params = [], bool|string $scheme = false):string {
 		return Url::to(self::GetActionUrl($action, $params), $scheme);
+	}
+
+	/**
+	 * Альтернативный способ получения ссылки в созданном экземпляре контроллера.
+	 * Метод учитывает модуль контроллера (если тот импортирует ModuleTrait).
+	 * @param string $action
+	 * @param array $params
+	 * @param bool|string $scheme
+	 * @return string
+	 */
+	public function link(string $action, array $params = [], bool|string $scheme = false):string {
+		/** @var Controller $this */
+		return (method_exists($this->module, 'to'))
+			?$this->module::to(self::GetActionUrl($action, $params))
+			:Url::to(self::GetActionUrl($action, $params), $scheme);
 	}
 
 	/**
