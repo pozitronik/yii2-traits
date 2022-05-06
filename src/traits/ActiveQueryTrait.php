15b8d6f183bb0cd31e4926aae87740587281ca3c
Pozitronik <antikillerxxl@gmail.com>
2022-05-06 16:01:46 +0300

    new methods

--- src/traits/ActiveQueryTrait.php
+++ src/traits/ActiveQueryTrait.php
@@ -3,6 +3,8 @@ declare(strict_types = 1);
 
 namespace pozitronik\traits\traits;
 
+use yii\base\NotSupportedException;
+use yii\db\ActiveQueryInterface;
 use yii\db\ActiveQueryTrait as YiiActiveQueryTrait;
 use yii\db\ActiveRecord;
 
@@ -24,6 +26,34 @@ trait ActiveQueryTrait {
 		$class = new $this->modelClass();//Хак для определения вызывающего трейт класса (для определения имени связанной таблицы)
 		$tableName = $class::tableName();
 		return $class->hasAttribute('deleted')?$this->andOnCondition([$tableName.'.deleted' => $deleted]):$this;
+	}
+
+	/**
+	 * Выборка по условию, включающая релейшен только тогда, когда выборка активна.
+	 * Нужна для оптимизации поиска по hasMany-связям, для того, чтобы не использовать distinct() без нужды.
+	 * @param array $condition
+	 * @param string|array $relation
+	 * @param bool $eagerLoading
+	 * @param string $joinType
+	 * @param bool $distinct
+	 * @return ActiveQueryInterface
+	 * @throws NotSupportedException
+	 */
+	public function andFilterWhereRelation(array $condition, string|array $relation, bool $eagerLoading = true, string $joinType = 'LEFT JOIN', bool $distinct = true):ActiveQueryInterface {
+		/** @var ActiveQueryInterface $this */
+		if ([] !== $condition = $this->filterCondition($condition)) {
+			$this->joinWith($relation, $eagerLoading, $joinType);
+			$this->andWhere($condition);
+			if ($distinct) $this->distinct();
+		}
+		return $this;
+	}
+
+	/**
+	 * inheritDoc
+	 */
+	public function isEmpty($value):bool {
+		return '' === $value || $value === [] || null === $value || (is_string($value) && '' === trim($value));
 	}
 
 }
